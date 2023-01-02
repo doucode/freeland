aa1c2e7d5695ccc0de0d7e008ccdb086d176efe2
DouCode <=>
2023-01-03 00:02:08 +0800

    gateway

--- user/src/main/java/com/douxiangwei/user/UserApplication.java
+++ user/src/main/java/com/douxiangwei/user/UserApplication.java
@@ -3,6 +3,7 @@ package com.douxiangwei.user;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 @RefreshScope
